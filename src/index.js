05cfd0c551ba09ef78b4f0da752b1ef8f3736f58
Gagan <gagan7535@gmail.com>
2020-10-09 17:37:44 +0530

    list

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ const { Map } = require("immutable");
 const map1 = Map({ a: 1, b: 2, c: 3 });
 const map2 = map1.set("b", 50);
 console.log(map1.get("b") === map2.get("b"));
+
+const { List } = require("immutable");
+
+const list1 = List([1, 2, 3]);
+
+const list2 = list1;
+
+const list3 = list2.push(5);
+
+console.log(list1);
+console.log(list3);
+console.log(list2);
